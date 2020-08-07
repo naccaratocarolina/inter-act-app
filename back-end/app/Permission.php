03a08c62bc589982f85acea3b5077943788d96ad
naccaratocarolina <naccaratocarolina@gmail.com>
2020-08-07 19:45:04 -0300

    + Controllers de Role e Permission

--- back-end/app/Permission.php
+++ back-end/app/Permission.php
@@ -4,7 +4,16 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Role;
+
 class Permission extends Model
 {
-    //
+  /**
+   * Many to Many Relationship Role & Permission
+   * A Role can have n Permissions
+   * A Permission can be assigned to n Roles
+   */
+    public function roles() {
+      return $this->belongsToMany('App\Role', 'roles_permissions');
+    }
 }
--- back-end/app/Role.php
+++ back-end/app/Role.php
@@ -4,7 +4,16 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Permission;
+
 class Role extends Model
 {
-    //
+  /**
+   * Many to Many Relationship Role & Permission
+   * A Role can have n Permissions
+   * A Permission can be assigned to n Roles
+   */
+    public function permissions() {
+      return $this->belongsToMany('App\Permission', 'roles_permissions');
+    }
 }
