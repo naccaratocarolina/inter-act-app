7e7147d4955690da5f8192c946152e41008bd96f
naccaratocarolina <naccaratocarolina@gmail.com>
2020-08-17 21:16:20 -0300

    + integracao artigo deletar

--- front-end/src/app/components/article-profile/article-profile.component.ts
+++ front-end/src/app/components/article-profile/article-profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
+import { ProfilePage } from '../../pages/profile/profile.page';
 
 @Component({
   selector: 'app-article-profile',
@@ -8,13 +9,28 @@ import { Router } from '@angular/router';
 })
 export class ArticleProfileComponent implements OnInit {
   @Input() All;
-  constructor( private router:Router ) { }
+  profile_id:number;
+
+  constructor( private router:Router, public profilePage:ProfilePage ) {
+    this.profile_id = JSON.parse(localStorage.getItem('profile_id'));
+  }
 
   ngOnInit() {}
+
+  //Funcao que deleta um artigo
+  public destroyArticle(article_id, profile_id) {
+    this.profilePage.destroyArticle(article_id, profile_id);
+  }
 
   //Redireciona para a pagina do artigo carregando o id do artigo clicado
   public redirectArticle(article_id) {
     localStorage.setItem('article_id', JSON.stringify(article_id));
     this.router.navigate(['/article']);
+  }
+
+  //Redireciona para a pagina de edicao de artigo
+  public redirectEditArticle(article_id) {
+    localStorage.setItem('article_id', JSON.stringify(article_id));
+    this.router.navigate(['/edit-article']);
   }
 }
